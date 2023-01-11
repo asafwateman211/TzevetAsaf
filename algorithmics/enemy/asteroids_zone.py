1d765ed39dc87c3862a44dd6ddfdbf2bc1496b84
Tseela <tseelagamzo@gmail.com>
2023-01-11 14:39:49 +0200

    removed unused parts

--- algorithmics/enemy/asteroids_zone.py
+++ algorithmics/enemy/asteroids_zone.py
@@ -13,16 +13,3 @@ class AsteroidsZone(Enemy):
 
     def get_vertices(self):
         return self.boundary
-
-    def get_edges(self):
-        return geo.LineString(self.boundary)
-
-    # Checks weather the point is inside the enemy
-    def is_in_enemy(self, point):
-        return self.polygon.contains(point)
-
-    def is_path_valid(self, start_coordinate, target_coordinate):
-        line = geo.LineString([start_coordinate, target_coordinate])
-        intersection = self.polygon.intersection(line)
-        return not(intersection.isempty())
-
--- algorithmics/enemy/enemy.py
+++ algorithmics/enemy/enemy.py
@@ -6,18 +6,3 @@ class Enemy(ABC):
     # each vertex is type coordinate
     def get_vertices(self):
         pass
-
-    # returns list of (edge1, edge2)
-    # each couple is an edge in Graph
-    def get_edges(self):
-        pass
-
-    # returns true coordinate is in enemy territory
-    # else, returns false
-    def is_in_enemy(self, coordinate):
-        pass
-
-    # returns true if (start_coordinate -> target_coordinate) is a valid path
-    # else, returns false
-    def is_path_valid(self, start_coordinate, target_coordinate):
-        pass
