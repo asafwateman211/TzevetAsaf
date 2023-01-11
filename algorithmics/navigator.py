7b8c026bd76035c8b3ddccb9511c7d9dac036f93
Tseela <tseelagamzo@gmail.com>
2023-01-11 13:22:13 +0200

    visgraph now can handle with polys that cut each other

--- algorithmics/navigator.py
+++ algorithmics/navigator.py
@@ -1,9 +1,13 @@
 from typing import List, Tuple
 
 import networkx as nx
+import pyvisgraph
+from pyvisgraph import vis_graph as vg, graph
 
+'''
 from algorithmics.enemy.enemy import Enemy
 from algorithmics.utils.coordinate import Coordinate
+from pyvisgraph import vis_graph as vg
 
 
 # Navigator
@@ -22,3 +26,11 @@ def calculate_path(source: Coordinate, targets: List[Coordinate], enemies: List[
     :return: list of calculated path waypoints and the graph constructed
     """
     return [source] + targets, nx.DiGraph()
+'''
+
+polys = [[graph.Point(0.0, 5.0), graph.Point(5.0, 5.0), graph.Point(5, 0), graph.Point(0, 0)],
+         [graph.Point(4, 4), graph.Point(8, 4.0), graph.Point(4, 8.0), graph.Point(8, 8)]]
+g = vg.VisGraph()
+g.build(polys)
+shortest = g.shortest_path(graph.Point(0, 0.0), graph.Point(9, 9))
+print(shortest)
