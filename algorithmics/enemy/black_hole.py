2f8cbb65968985427bf724d2a0e7a20922931883
Tseela <tseelagamzo@gmail.com>
2023-01-11 10:23:41 +0200

    added interval attribute

--- algorithmics/enemy/black_hole.py
+++ algorithmics/enemy/black_hole.py
@@ -12,3 +12,22 @@ class BlackHole(Enemy):
         """
         self.center = center
         self.radius = radius
+        self.depth_interval = radius / 2
+
+    def set_vertices_depth_interval(self, interval):
+        self.depth_interval = interval
+
+    # returns list of vertices for Graph
+    # each vertex is type coordinate
+    def get_vertices(self):
+        pass
+
+    # returns list of (edge1, edge2)
+    # each couple is an edge in Graph
+    def get_edges(self):
+        pass
+
+    # returns true if (start_coordinate -> target_coordinate) is a valid path
+    # else, returns false
+    def is_path_valid(self, start_coordinate, target_coordinate):
+        pass
