b0f0e0b9eda1e6fb3c65585d8b159a8dfece0fd5
Tseela <tseelagamzo@gmail.com>
2023-01-11 09:56:59 +0200

    added functions in enemy.py. need to be implemented in inheritance classes.

--- algorithmics/enemy/enemy.py
+++ algorithmics/enemy/enemy.py
@@ -2,4 +2,17 @@ from abc import ABC
 
 
 class Enemy(ABC):
-    pass
+    # returns list of vertices for Graph
+    # each vertex is type coordinate
+    def get_vertices(self):
+        pass
+
+    # returns list of (edge1, edge2)
+    # each couple is an edge in Graph
+    def get_edges(self):
+        pass
+
+    # returns true if (start_coordinate -> target_coordinate) is a valid path
+    # else, returns false
+    def is_path_valid(self, start_coordinate, target_coordinate):
+        pass
